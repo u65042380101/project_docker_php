26fcf184b1bc55dd6fa04f1107899ed3c56fca02
u65042380101 <pqjbsqqq@gmail.com>
2025-06-13 11:45:16 +0700

    เเก้ไข error

--- member_list.php
+++ member_list.php
@@ -3,12 +3,12 @@
                 //1. เชื่อมต่อ database:
                 include('condb.php');  //ไฟล์เชื่อมต่อกับ database ที่เราได้สร้างไว้ก่อนหน้าน้ี
                 //2. query ข้อมูลจากตาราง tb_admin:
-                $query = "SELECT * FROM tbl_member ORDER BY member_id ASC" or die("Error:" . mysqli_error());
+                $query = "SELECT * FROM tbl_member ORDER BY member_id ASC" or die("Error:");
                 //3.เก็บข้อมูลที่ query ออกมาไว้ในตัวแปร result .
                 $result = mysqli_query($con, $query);
                 //4 . แสดงข้อมูลที่ query ออกมา โดยใช้ตารางในการจัดข้อมูล:
                 echo ' <table class="table table-hover">';
-                  //หัวข้อตาราง 
+                  //หัวข้อตารางง
                     echo "
                       <tr>
                       <td>id</td>
